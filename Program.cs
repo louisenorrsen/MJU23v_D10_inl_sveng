a6f6c1ce5a6ca5f8181c19b31adbfa54afd00dbf
louisenorrsen <93458801+louisenorrsen@users.noreply.github.com>
2023-03-24 20:26:54 +0100

    felhanterar 'delete' om användaren inte laddar in en fil först alternativt om ordet inte existera i listan.

--- Program.cs
+++ Program.cs
@@ -51,8 +51,8 @@
 
                     } else Console.WriteLine("The dictionary is empty, please load a file first!");
                 }
-                else if (command == "new") // FIXME: System.IndexOutOfRangeException - om input är 'new sol'
-                {
+                else if (command == "new")
+                { 
                     try
                     {
                         if (argument.Length > 1) AddNewGlossToDictionary(argument[1], argument[2]);
@@ -71,12 +71,19 @@
                 else if (command == "delete") // FIXME: System.ArgumentOutOfRangeException - om delete innan load
                                               // System.ArgumentOutOfRangeException - om ordet inte finns
                 { // TBD: Känn av motsvarande ord automatiskt
-                    if (argument.Length > 1) DeleteMatchingWords(argument[1], argument[2]);
-                    else
+                    try
                     {
-                        string swedishWord = AskForSwedishWord();
-                        string englishWord = AskForEnglishWord();
-                        DeleteMatchingWords(swedishWord, englishWord);
+                        if (argument.Length > 1) DeleteMatchingWords(argument[1], argument[2]);
+                        else
+                        {
+                            string swedishWord = AskForSwedishWord();
+                            string englishWord = AskForEnglishWord();
+                            DeleteMatchingWords(swedishWord, englishWord);
+                        }
+                    }
+                    catch (System.ArgumentOutOfRangeException ex)
+                    {
+                        Console.WriteLine("The word was not found, please enter another word\n" + ex.Message);
                     }
                 }
                 else if (command == "translate")
@@ -113,8 +120,8 @@
                 SweEngGloss gloss = dictionary[i];
                 if (gloss.word_swe == swedishWord && gloss.word_eng == englishWord) index = i;
             }
-            Console.WriteLine($"Words '{swedishWord}' and '{englishWord}' was deleted");
             dictionary.RemoveAt(index);
+            Console.WriteLine($"Words '{swedishWord}' and '{englishWord}' was deleted");
         }
 
         private static void AddNewGlossToDictionary(string swedishWord, string englishWord)
@@ -125,7 +132,8 @@
 
         private static void LoadFileWithWords(string fileName)
         {
-            using (StreamReader streamReader = new StreamReader($"..\\..\\..\\dict\\{fileName}"))
+            using (StreamReader streamReader = new StreamReader($"..\\..\\..\\dict\\{fileName}")) 
+                // FIXME: Ändra tillbaka så användaren själv får välja vart filen ligger
             {
                 dictionary.Clear();
                 string line = streamReader.ReadLine();
@@ -139,13 +147,13 @@
             Console.WriteLine("File was readed correctly!");
         }
 
-        private static string AskForEnglishWord()
+        private static string AskForEnglishWord() // FIXME: Ändra så att det inte går att lägga till siffror i orden
         {
             Console.Write("Write word in English: ");
             return Console.ReadLine().Trim();
         }
 
-        private static string AskForSwedishWord()
+        private static string AskForSwedishWord() // FIXME: Ändra så att det inte går att lägga till siffror i orden
         {
             Console.WriteLine("Write word in Swedish: ");
             return Console.ReadLine().Trim();
